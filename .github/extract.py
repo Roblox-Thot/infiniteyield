4620de25ad1b74c7406f568acabc38b4af2e2598
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2025-09-29 06:53:09 -0400

    test

---
+++ .github/extract.py
@@ -0,0 +1,24 @@
+lines = ''
+with open('source.txt', 'r') as file:
+    lines = file.readlines()
+
+replacements = {
+    "CMDs[#CMDs + 1] = {NAME = '": '',
+    "'}": '',
+
+    # Fuck ass Windows fix
+    '\n': '',
+    '\t': ''
+}
+
+out = open("out.md", 'w')
+for i in range(len(lines)):
+    if 'CMDs[#CMDs + 1] = {NAME = ' in lines[i]:
+        testline = lines[i]
+        for old, new in replacements.items():
+            testline = testline.replace(old, new)
+        name, desc = testline.split('\', DESC = \'')
+        if (name or desc) == "": continue
+        print(f'Command Name: {name}, Description: {desc}')
+        out.write(f'## {name}\n{desc}\n\n')
+out.close()
