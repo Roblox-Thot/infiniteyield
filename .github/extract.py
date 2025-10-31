b10901c93d27e7628b48000bbee6ab5d7ea81742
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2025-10-31 18:16:04 -0400

    Improve formatting in commands.md output
    Added a newline in the output file header for better formatting.

--- .github/extract.py
+++ .github/extract.py
@@ -6,7 +6,7 @@ replacements = {
 }
 
 out = open("commands.md", 'w')
-out.write(f'# Commands')
+out.write(f'# Commands\n\n')
 for i in range(len(lines)):
     if 'CMDs[#CMDs + 1] = {NAME = ' in lines[i]:
         command_line = lines[i]
@@ -24,5 +24,6 @@ for i in range(len(lines)):
         out.write(f'## {name}\n{desc}\n\n')
 
 out.close()
+
 
 
